a8667106d6d96a214d4775d3efa1df7d72a96642
Burhanuddin Salim <burhan.salman@gmail.com>
2025-05-28 12:06:30 +0100

    feat: add fetching orgs from gov.uk

--- scripts/populate.ts
+++ scripts/populate.ts
@@ -1,9 +1,13 @@
-import * as fs from "fs";
 import csv from "csv-parser";
 import format from "pg-format";
 import { Pool } from "pg";
+import axios from "axios";
+import * as cheerio from "cheerio";
+import { Readable } from "stream";
 
 async function upsertOrganisations(organisations: Record<string, string>[]) {
+  console.log("Upserting organisations...");
+
   const values = organisations.map((org) => [
     org["Organisation Name"],
     org["Town/City"],
@@ -21,7 +25,7 @@ async function upsertOrganisations(organisations: Record<string, string>[]) {
     values,
   );
 
-  const pool = new Pool({});
+  const pool = new Pool({ connectionString: process.env.DB_CONN_STRING });
   const client = await pool.connect();
 
   await client.query("BEGIN");
@@ -33,18 +37,66 @@ async function upsertOrganisations(organisations: Record<string, string>[]) {
     throw e;
   } finally {
     client.release();
+    pool.end();
   }
+
+  console.log(`Upserted ${values.length} organisations successfully.`);
 }
 
-const results: Record<string, string>[] = [];
+async function addIngestLog(url) {
+  console.log("Adding ingest log...");
 
-fs.createReadStream("organizations.csv")
-  .pipe(csv())
-  .on("data", (data) => {
-    results.push(data);
-  })
-  .on("end", () =>
-    upsertOrganisations(results).then(() =>
-      console.log("Data inserted successfully"),
-    ),
-  );
+  const pool = new Pool({ connectionString: process.env.DB_CONN_STRING });
+  const client = await pool.connect();
+
+  await client.query("BEGIN");
+  try {
+    await client.query("INSERT INTO public.ingest (url) VALUES ($1)", [url]);
+    await client.query("COMMIT");
+  } catch (e) {
+    client.query("ROLLBACK");
+    throw e;
+  } finally {
+    client.release();
+    pool.end();
+  }
+
+  console.log("Ingest log added successfully.");
+}
+
+async function main() {
+  console.log("Downloading latest CSV...");
+
+  const pageUrl =
+    "https://www.gov.uk/government/publications/register-of-licensed-sponsors-workers";
+  const selector =
+    "#documents > section > div.gem-c-attachment__details > h3 > a";
+
+  const pageHtml = await axios.get(pageUrl);
+
+  console.log("Parsing page for CSV URL...");
+
+  const $ = cheerio.load(pageHtml.data);
+  const csvUrl = $(selector).attr("href");
+
+  if (!csvUrl) {
+    throw new Error("CSV URL not found on the page");
+  }
+
+  console.log("CSV URL found");
+
+  console.log("Downloading CSV file...");
+  const fileStream = await axios.get<Readable>(csvUrl, {
+    responseType: "stream",
+  });
+
+  const results: Record<string, string>[] = [];
+  fileStream.data
+    .pipe(csv())
+    .on("data", (data: Record<string, string>) => results.push(data))
+    .on("end", () =>
+      upsertOrganisations(results).then(() => addIngestLog(csvUrl)),
+    );
+}
+
+main();
