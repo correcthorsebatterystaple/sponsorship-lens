242c02621772bcea0d5683750a28d34e7e5fa896
Burhanuddin Salim <burhan.salman@gmail.com>
2025-05-29 22:34:26 +0100

    feat: make results summary sticky

--- src/components/ResultsSummary.tsx
+++ src/components/ResultsSummary.tsx
@@ -1,7 +1,7 @@
 type ResultsSummaryProps = { total: number; showing: number };
 const ResultsSummary: React.FC<ResultsSummaryProps> = ({ total, showing }) => {
   return (
-    <div className="px-4 py-2 text-gray-500 text-sm">
+    <div className="px-4 py-2 text-gray-500 text-sm sticky top-17 bg-white">
       {` Showing top ${Math.min(showing, total)} of ${total} results found`}
     </div>
   );
