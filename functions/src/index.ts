37a51daa6b82766ff4f18e93cb06bf790d4e53c1
Burhanuddin Salim <burhan.salman@gmail.com>
2025-05-29 21:59:40 +0100

    feat: add auth for firebase function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,20 @@
 import { onRequest } from "firebase-functions/v2/https";
 import { DB_CONN_STRING } from "./update-orgs/utils";
 import { main } from "./update-orgs";
+import { defineSecret } from "firebase-functions/params";
+
+const SECRET_HEADER = defineSecret("SECRET_HEADER");
 
 export const updateOrgs = onRequest(
-  { secrets: [DB_CONN_STRING], memory: "512MiB" },
-  async (_, response) => {
+  { secrets: [DB_CONN_STRING, SECRET_HEADER], memory: "512MiB" },
+  async (request, response) => {
+    if (request.headers["x-secret-header"] !== SECRET_HEADER.value()) {
+      response.status(403).send("Forbidden");
+      return;
+    }
+
     await main();
+
     response.send("OK");
   },
 );
