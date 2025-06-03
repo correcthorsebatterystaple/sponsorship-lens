aedfb6429ac4f3bdc15affae67eddbd1194eb8ab
Burhanuddin Salim <burhan.salman@gmail.com>
2025-06-03 23:24:25 +0100

    chore: reduce bundle size for functions
    Firebase installs all node dependencies q

--- functions/esbuild.mjs
+++ functions/esbuild.mjs
@@ -2,9 +2,9 @@ import * as esbuild from "esbuild";
 
 await esbuild.build({
   entryPoints: ["./src/index.ts"],
-  bundle: true,
-  sourcemap: "inline",
+  bundle: false,
+  minify: true,
+  sourcemap: "external",
   platform: "node",
   outfile: "./lib/index.js",
-  external: ["firebase-functions", "pg-format"],
 });
